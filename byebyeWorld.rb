f5468c6673beae2ac53b0ee9318ccd1979f27d98
wyllman <wyllman@gmail.com>
2014-10-10 15:04:04 +0100

    Creado el programa byebyeWorld.rb en Ruby para usar con Pry

---
+++ byebyeWorld.rb
@@ -0,0 +1,15 @@
+#bybyeWorld.rb
+require 'pry'
+
+#define a method
+def byebye() puts "bye bye world!!" end
+
+#set x to 10
+x = 10
+
+#start a PEPL session
+binding.pry
+
+#program resumes here (after pry session)
+puts "program resumes here. Value of x is: #{x}."
+
